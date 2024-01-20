aa359da4f4dfe3972db62f7ddfd16f74030b7e00
Konstantyn <gagarin2789@gmail.com>
2024-01-20 01:03:30 +0100

    Added RegistrationScreen

---
+++ app/src/main/java/com/clipie/ui/RegistrationScreen.kt
@@ -0,0 +1,90 @@
+package com.clipie.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.clipie.ui.theme.lobsterFontFamily
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination(start = true)
+@Composable
+fun RegistrationScreen() {
+    var email by rememberSaveable {
+        mutableStateOf("")
+    }
+    var username by rememberSaveable {
+        mutableStateOf("")
+    }
+    var password by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Spacer(modifier = Modifier.height(35.dp))
+        Text(
+            text = "Clipie",
+            fontFamily = lobsterFontFamily,
+            fontSize = 60.sp,
+            color = Color(38, 38, 38)
+        )
+        Text(
+            text = "Sign up to see photos and videos\n from your friends.",
+            color = Color(142, 144, 146),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.W600
+        )
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it},
+            label = { Text(text = "Email") },
+        )
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = {Text(text = "Username")},
+
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Password") },
+        )
+    }
+}
+
+@Composable
+fun CustomOutlinedTextField(
+    value: String,
+    label: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = {Text(text = label)}
+    )
+}
+
