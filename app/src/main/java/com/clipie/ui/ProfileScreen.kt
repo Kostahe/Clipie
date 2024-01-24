aa9a23a99a2cf11d18e72a1c88161873126b4c09
Toiletman007-PC <quertical5@gmail.com>
2024-01-23 19:43:24 -0800

    Made the bottom sheet simpler and also made it work with the icon updating correctly

--- app/src/main/java/com/clipie/ui/ProfileScreen.kt
+++ app/src/main/java/com/clipie/ui/ProfileScreen.kt
@@ -3,9 +3,11 @@ package com.clipie.ui
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
@@ -13,21 +15,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.BottomAppBarDefaults.windowInsets
-import androidx.compose.material3.BottomSheetDefaults
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -37,7 +32,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -52,7 +46,6 @@ import androidx.compose.ui.unit.dp
 import com.clipie.R
 import com.clipie.ui.theme.Black
 import com.ramcosta.composedestinations.annotation.Destination
-import kotlinx.coroutines.launch
 
 
 @Destination(start = true)
@@ -114,17 +107,8 @@ fun ProfileScreenBottomBar(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreenTopBar() {
-    var expanded by rememberSaveable { mutableStateOf(false) }
-
-    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
-    var skipPartiallyExpanded by rememberSaveable { mutableStateOf(false) }
-    var edgeToEdgeEnabled by rememberSaveable { mutableStateOf(false) }
-
-    val scope = rememberCoroutineScope()
-    val bottomSheetState = rememberModalBottomSheetState(
-        skipPartiallyExpanded = skipPartiallyExpanded
-    )
-
+    val accountSheetState = rememberModalBottomSheetState()
+    var accountSheetOpenSwitch by rememberSaveable { mutableStateOf(false) }
     TopAppBar(title = {
 
         BadgedBox(badge = {
@@ -136,17 +120,7 @@ fun ProfileScreenTopBar() {
                 )
             }
         }) {
-            TextButton(onClick = {
-                Log.d("test", "1 TEST!!!!!!!")
-                scope.launch { bottomSheetState.hide() }.invokeOnCompletion {
-                    if (!bottomSheetState.isVisible) {
-                        openBottomSheet = false
-                    }
-                }
-
-                Log.d("test", "2 TEST!!!!!!!")
-            }
-            ) {
+            TextButton(onClick = { accountSheetOpenSwitch = true }) {
                 Text(
                     text = "User name here",
                     style = MaterialTheme.typography.headlineLarge,
@@ -154,112 +128,68 @@ fun ProfileScreenTopBar() {
                     color = Black
                 )
                 Icon(
-                    imageVector = if (expanded) TopListOfItems[3].selectedIcon else TopListOfItems[3].unselectedIcon,
+                    imageVector = if (accountSheetOpenSwitch) TopListOfItems[3].selectedIcon else TopListOfItems[3].unselectedIcon,
                     contentDescription = TopListOfItems[3].title,
                     modifier = Modifier.padding(start = 3.dp, top = 5.dp)
                 )
             }
         }
-        data class account(
+        data class Account(
             val pfp: Painter,
             val name: String,
             val notificationCount: Int,
-        ) {}
+        )
 
         val imagePainter: Painter = painterResource(id = R.drawable.temp_acc_pfp)
 
-        val userAccountList: List<account> = listOf(
-            account(
+        val userAccountList: List<Account> = listOf(
+            Account(
                 imagePainter,
-                "franta",
+                "Franta",
                 69
+            ),
+            Account(
+                painterResource(id = R.drawable.ic_launcher_background),
+                "vlad",
+                2
             )
         )
-        ModalBottomSheet(
-            onDismissRequest = { openBottomSheet = false },
-            sheetState = bottomSheetState,
-            windowInsets = windowInsets
+        if (accountSheetOpenSwitch) {
 
-        ) {
-            if(openBottomSheet){
-                val windowInsets = if (edgeToEdgeEnabled)
-                    WindowInsets(0) else BottomSheetDefaults.windowInsets
-            }
-            LazyColumn(
-                modifier = Modifier
+
+            ModalBottomSheet(
+                onDismissRequest = { accountSheetOpenSwitch = false },
+                sheetState = accountSheetState,
+            ) {
+//                Selected account
+                Row(modifier = Modifier
+                    .clickable { Log.d("rowclick", "Row has been clicked!") }
                     .fillMaxWidth()
-                    .fillMaxHeight(0.4f),
-                horizontalAlignment = Alignment.Start,
-
+                    .offset()
+                    .fillMaxHeight(0.1f),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-//                Selected profile
-                item {
-                    Button(onClick = { /*TODO*/ }) {
-                        Row(
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            Image(
-                                painter = userAccountList[0].pfp,
-                                contentDescription = null,
-                                modifier = Modifier
-                                    .size(100.dp)
-                                    .clip(CircleShape)
-                            )
-                            Text(text = userAccountList[0].name)
-                            Text(text = userAccountList[0].notificationCount.toString() + "Notifications")
-
-                        }
-                    }
-
-                }
-//                All unSelected profiles
-//                if (Profile.Count > 1){
-//                    display other profiles
-//                }
-                item {
-                    Button(onClick = { /*TODO*/ }) {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.AccountCircle,
-                                contentDescription = null,
-                                modifier = Modifier.size(60.dp)
-                            )
-                            Text(text = "Add Account")
-                            Text(text = userAccountList[0].notificationCount.toString() + "Notifications")
-                        }
-                    }
-
-
-                }
-
-//                    Add Account
-
-                item() {
-                    OutlinedButton(
-                        onClick = { /*TODO*/ }, modifier = Modifier
-                            .fillMaxWidth()
-                            .border(
-                                2.dp, Color.White, RoundedCornerShape(50.dp)
-
-                            )
-                    ) {
-                        Row(
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.AccountCircle,
-                                contentDescription = null,
-                                modifier = Modifier.size(59.dp)
-                            )
-                            Text(text = "Add Account")
-                            Text(text = userAccountList[0].notificationCount.toString() + "Notifications")
-                        }
-                    }
+                    Spacer(modifier = Modifier.size(20.dp))
+                    Image(
+                        painter = imagePainter,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(65.dp)
+                            .clip(CircleShape)
+                            .border(1.dp, Color.Gray, CircleShape)
+                    )
+                    Text(
+                        text = userAccountList[0].name,
+                        modifier = Modifier.offset(x = 20.dp),
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.Bold,
+                    )
                 }
             }
+//                Collumn of unselected accounts
+
+
+
         }
     },
         actions = {
@@ -302,4 +232,3 @@ fun ProfileScreenTopBar() {
         }
     )
 }
-
