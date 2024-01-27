58041a2e9ae14e4e7a0c1e2f545972ca2ec96d05
Konstantyn <gagarin2789@gmail.com>
2024-01-27 20:07:29 +0100

    Created FireBaseModule

---
+++ app/src/main/java/com/clipie/di/FireBaseModule.kt
@@ -0,0 +1,19 @@
+package com.clipie.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+object FireBaseModule {
+
+    @Provides
+    @Singleton
+    fun provideFireStore() = FirebaseFirestore.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideAuthentication() = FirebaseAuth.getInstance()
+}
