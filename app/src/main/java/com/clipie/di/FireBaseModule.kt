2bafc575b9b58d00044a0f83d4788a486b05fd37
Konstantyn <gagarin2789@gmail.com>
2024-01-27 20:09:38 +0100

    Created FireBaseModule and provided FireStore and Authentication

--- app/src/main/java/com/clipie/di/FireBaseModule.kt
+++ app/src/main/java/com/clipie/di/FireBaseModule.kt
@@ -4,9 +4,12 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 object FireBaseModule {
 
     @Provides
