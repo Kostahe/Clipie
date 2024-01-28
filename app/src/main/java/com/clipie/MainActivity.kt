f8c88f06c5c4150bf3d3b5a89e8ab6c7d449b972
Konstantyn <gagarin2789@gmail.com>
2024-01-28 19:36:14 +0100

    Added basic navigation animation

--- app/src/main/java/com/clipie/MainActivity.kt
+++ app/src/main/java/com/clipie/MainActivity.kt
@@ -3,16 +3,34 @@ package com.clipie
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.ui.Alignment
 import com.clipie.presentation.NavGraphs
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.animations.defaults.NestedNavGraphDefaultAnimations
+import com.ramcosta.composedestinations.animations.defaults.RootNavGraphDefaultAnimations
+import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterialNavigationApi::class, ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DestinationsNavHost(navGraph = NavGraphs.root)
+            val navHostEngine = rememberAnimatedNavHostEngine(
+                navHostContentAlignment = Alignment.TopCenter,
+                rootDefaultAnimations = RootNavGraphDefaultAnimations.ACCOMPANIST_FADING,
+                defaultAnimationsForNestedNavGraph = mapOf(
+                    NavGraphs.root to NestedNavGraphDefaultAnimations(
+                        enterTransition = { slideInHorizontally() },
+                        exitTransition = { slideOutHorizontally() }
+                    ),
+                ))
+            DestinationsNavHost(navGraph = NavGraphs.root, engine = navHostEngine)
         }
     }
 }
