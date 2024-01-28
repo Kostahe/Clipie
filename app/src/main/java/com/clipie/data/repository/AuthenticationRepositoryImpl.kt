3b00ed53878479ed798a8fca9ab761d624a342ab
Konstantyn <gagarin2789@gmail.com>
2024-01-28 19:09:00 +0100

    Implemented singUp and updateUser functions

--- app/src/main/java/com/clipie/data/repository/AuthenticationRepositoryImpl.kt
+++ app/src/main/java/com/clipie/data/repository/AuthenticationRepositoryImpl.kt
@@ -3,15 +3,9 @@ package com.clipie.data.repository
 import com.clipie.domain.entities.State
 import com.clipie.domain.model.User
 import com.clipie.domain.repository.AuthenticationRepository
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
-import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
 
 class AuthenticationRepositoryImpl @Inject constructor(
     private val authentication: FirebaseAuth,
@@ -43,7 +37,7 @@ class AuthenticationRepositoryImpl @Inject constructor(
 
     override fun updateUser(user: User, result: (State<Unit>) -> Unit)  {
         val document = database.collection("user").document(user.id)
-        document.set(user).addOnSuccessListener { task ->
+        document.set(user).addOnSuccessListener {
             result.invoke(State.Success(Unit))
         }.addOnFailureListener {
             result.invoke(State.Error(it.localizedMessage?.toString() ?: "Firebase Error"))
