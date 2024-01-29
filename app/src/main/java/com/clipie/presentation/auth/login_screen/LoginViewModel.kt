d89e99182aeaa36dce7e70ace1c92fb93eb2443a
Konstantyn <gagarin2789@gmail.com>
2024-01-29 20:36:18 +0100

    Changed location of all authentication files

--- app/src/main/java/com/clipie/presentation/auth/login_screen/LoginViewModel.kt
+++ app/src/main/java/com/clipie/presentation/auth/login_screen/LoginViewModel.kt
@@ -1,4 +1,4 @@
-package com.clipie.presentation.login_screen
+package com.clipie.presentation.auth.login_screen
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
