83ac51c14e0feb2712c83e212fd52e6596e5b548
Konstantyn <gagarin2789@gmail.com>
2024-01-31 20:14:12 +0100

    Updated layout in RegistrationScreen

--- app/src/main/java/com/clipie/presentation/screens/auth/registration_screen/components/RegistrationScreen.kt
+++ app/src/main/java/com/clipie/presentation/screens/auth/registration_screen/components/RegistrationScreen.kt
@@ -1,11 +1,14 @@
 package com.clipie.presentation.screens.auth.registration_screen.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,18 +17,24 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.clipie.ui.theme.lobsterFontFamily
+import com.clipie.R
+import com.clipie.presentation.screens.auth.components.AuthenticationButton
+import com.clipie.presentation.screens.auth.components.AuthenticationPasswordTextField
+import com.clipie.presentation.screens.auth.components.AuthenticationTextField
+import com.clipie.ui.theme.Background
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Destination
 @Composable
 fun RegistrationScreen(
-    navController: DestinationsNavigator
+    modifier: Modifier = Modifier,
+    navigator: DestinationsNavigator
 ) {
     var email by rememberSaveable {
         mutableStateOf("")
@@ -36,40 +45,54 @@ fun RegistrationScreen(
     var password by rememberSaveable {
         mutableStateOf("")
     }
-
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Background)
     ) {
-        Spacer(modifier = Modifier.height(35.dp))
         Text(
-            text = "Clipie",
-            fontFamily = lobsterFontFamily,
-            fontSize = 60.sp,
-            color = Color(38, 38, 38)
+            text = stringResource(R.string.create_new_account),
+            modifier = Modifier.padding(end = 100.dp, top = 20.dp, start = 30.dp),
+            fontSize = 40.sp,
+            maxLines = 2
         )
-        Text(
-            text = "Sign up to see photos and videos\n from your friends.",
-            color = Color(142, 144, 146),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.W700
+        Spacer(modifier = Modifier.height(170.dp))
+        AuthenticationTextField(
+            value = "",
+            onValueChange = {},
+            label = stringResource(R.string.email),
+            imeAction = ImeAction.Next
         )
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it},
-            label = { Text(text = "Email") },
+        AuthenticationTextField(
+            value = "",
+            onValueChange = {},
+            label = stringResource(R.string.username),
+            imeAction = ImeAction.Done,
+            modifier = modifier.padding(top = 5.dp),
         )
-        OutlinedTextField(
-            value = username,
-            onValueChange = { username = it },
-            label = {Text(text = "Username")},
-
+        AuthenticationPasswordTextField(
+            value = "",
+            onValueChange = {},
+            label = stringResource(R.string.password),
+            imeAction = ImeAction.Done,
+            modifier = modifier.padding(top = 5.dp),
         )
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text(text = "Password") },
+        AuthenticationButton(
+            modifier = Modifier
+                .width(275.dp)
+                .padding(10.dp)
+            , onClick = {},
+            text = stringResource(id = R.string.create_account)
         )
+        TextButton(
+            onClick = { navigator.navigateUp() },
+            modifier = Modifier.padding(top = 200.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.already_have_an_account),
+                color = Color(0xFF0064e0),
+            )
+        }
     }
 }
