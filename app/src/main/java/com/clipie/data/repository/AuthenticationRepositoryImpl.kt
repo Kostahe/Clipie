37be440d2b9b539188264ec50e51d5b5c852eb14
Konstantyn <gagarin2789@gmail.com>
2024-02-01 18:21:39 +0100

    Implemented login, storeSession, getSession functions

--- app/src/main/java/com/clipie/data/repository/AuthenticationRepositoryImpl.kt
+++ app/src/main/java/com/clipie/data/repository/AuthenticationRepositoryImpl.kt
@@ -1,18 +1,28 @@
 package com.clipie.data.repository
 
+import android.content.SharedPreferences
 import com.clipie.data.common.FireStoreTable
+import com.clipie.data.common.SharedPreferenceName
 import com.clipie.domain.entities.State
 import com.clipie.domain.model.User
 import com.clipie.domain.repository.AuthenticationRepository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.gson.Gson
 import javax.inject.Inject
 
 class AuthenticationRepositoryImpl @Inject constructor(
     private val authentication: FirebaseAuth,
-    private val database: FirebaseFirestore
+    private val fireStore: FirebaseFirestore,
+    private val sharedPreferences: SharedPreferences,
+    private val gson: Gson
 ) : AuthenticationRepository {
-    override fun createUser(email: String, password: String, user: User, result: (State<Unit>) -> Unit) {
+
+    override fun createUser(
+        email: String,
+        password: String,
+        user: User, result: (State<Unit>) -> Unit
+    ) {
         authentication.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
@@ -36,12 +46,63 @@ class AuthenticationRepositoryImpl @Inject constructor(
             }
     }
 
-    override fun updateUser(user: User, result: (State<Unit>) -> Unit)  {
-        val document = database.collection(FireStoreTable.USER.tableName).document(user.id)
-        document.set(user).addOnSuccessListener {
+    override fun updateUser(
+        user: User,
+        result: (State<Unit>) -> Unit
+    ) {
+        val documentRef = fireStore.collection(FireStoreTable.USER.tableName).document(user.id)
+        documentRef.set(user).addOnSuccessListener {
             result.invoke(State.Success(Unit))
         }.addOnFailureListener {
             result.invoke(State.Error(it.localizedMessage?.toString() ?: "Firebase Error"))
+        }
+    }
+
+    override fun login(
+        email: String,
+        password: String,
+        result: (State<Unit>) -> Unit
+    ) {
+        authentication.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                storeSession(task.result.user?.uid ?: "") { user ->
+                    if (user == null) {
+                        result.invoke(State.Error("Authentication succeed, but session didn't save user"))
+                    } else {
+                        result.invoke(State.Success(Unit))
+                    }
+                }
+            } else {
+                result.invoke(State.Error(task.exception.toString()))
+            }
+        }
+    }
+
+    override fun storeSession(
+        id: String,
+        result: (User?) -> Unit
+    ) {
+        val documentRef = fireStore.collection(FireStoreTable.USER.tableName).document(id)
+        documentRef.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val user = task.result?.toObject(User::class.java)
+                sharedPreferences.edit()
+                    .putString(SharedPreferenceName.USER_SESSION.preferenceName, gson.toJson(user))
+                    .apply()
+                result.invoke(user)
+            } else {
+                result.invoke(null)
+            }
+        }
+    }
+
+    override fun getSession(): User? {
+        val userString = sharedPreferences
+            .getString(SharedPreferenceName.USER_SESSION.preferenceName, null)
+        return if (userString != null) {
+            gson.fromJson(userString, User::class.java)
+        } else {
+            null
         }
     }
 }
