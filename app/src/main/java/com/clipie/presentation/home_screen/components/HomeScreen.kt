0f9f698d3fb0cd7ba5abd8844a457dc809a976e3
Konstantyn <gagarin2789@gmail.com>
2024-02-02 20:48:12 +0100

    Updated HomeScreen

--- app/src/main/java/com/clipie/presentation/home_screen/components/HomeScreen.kt
+++ app/src/main/java/com/clipie/presentation/home_screen/components/HomeScreen.kt
@@ -1,8 +1,9 @@
 package com.clipie.presentation.home_screen.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(modifier: Modifier = Modifier) {
 
 }
