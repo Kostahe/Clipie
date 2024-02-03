c049b4c0aff65164b089e640612e754eeb0f100d
Konstantyn <gagarin2789@gmail.com>
2024-02-03 18:44:54 +0100

    Added shared AuthenticationViewModel

--- app/src/main/java/com/clipie/presentation/navigation/app/AppNavHost.kt
+++ app/src/main/java/com/clipie/presentation/navigation/app/AppNavHost.kt
@@ -1,11 +1,17 @@
 package com.clipie.presentation.navigation.app
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.clipie.presentation.auth.AuthenticationViewModel
 import com.clipie.presentation.auth.login_screen.components.LoginScreen
 import com.clipie.presentation.auth.registration_screen.components.RegistrationScreen
 import com.clipie.presentation.main.main_screen.components.MainScreen
@@ -25,14 +31,25 @@ fun AppNavHost(
             route = AppNavConstant.Authentication.route
         ) {
             composable(route = AuthenticationNavConstant.Login.route) {
-                LoginScreen(navController = navController)
+                val viewModel = it.sharedViewModel<AuthenticationViewModel>(navController = navController)
+                LoginScreen(navController = navController, viewModel = viewModel)
             }
             composable(route = AuthenticationNavConstant.Register.route) {
-                RegistrationScreen(navController = navController)
+                val viewModel = it.sharedViewModel<AuthenticationViewModel>(navController = navController)
+                RegistrationScreen(navController = navController, viewModel = viewModel)
             }
         }
         composable(route = AppNavConstant.Main.route) {
             MainScreen()
         }
     }
+}
+
+@Composable
+inline fun <reified T : ViewModel> NavBackStackEntry.sharedViewModel(navController: NavHostController): T {
+    val navGraphRoute = destination.parent?.route ?: return hiltViewModel()
+    val parentEntry = remember(this) {
+        navController.getBackStackEntry(navGraphRoute)
+    }
+    return hiltViewModel(parentEntry)
 }
