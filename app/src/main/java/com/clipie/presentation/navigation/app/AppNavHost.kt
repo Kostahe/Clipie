c8a8d8e28d19bfcef10a533cbf8249b373aa1804
Konstantyn <gagarin2789@gmail.com>
2024-02-03 23:20:53 +0100

    Added AppNavHost

--- app/src/main/java/com/clipie/presentation/navigation/app/AppNavHost.kt
+++ app/src/main/java/com/clipie/presentation/navigation/app/AppNavHost.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -14,13 +13,7 @@ import androidx.navigation.navigation
 import com.clipie.presentation.auth.AuthenticationViewModel
 import com.clipie.presentation.auth.login_screen.components.LoginScreen
 import com.clipie.presentation.auth.registration_screen.components.RegistrationScreen
-import com.clipie.presentation.main.add_screen.components.AddScreen
-import com.clipie.presentation.main.home_screen.components.HomeScreen
 import com.clipie.presentation.main.main_screen.components.MainScreen
-import com.clipie.presentation.main.profile_screen.components.ProfileScreen
-import com.clipie.presentation.main.search_screen.components.SearchScreen
-import com.clipie.presentation.navigation.main.MainNavConstant
-import com.clipie.presentation.navigation.main.MainNavHost
 
 @Composable
 fun AppNavHost(
@@ -44,23 +37,10 @@ fun AppNavHost(
                 val viewModel = it.sharedViewModel<AuthenticationViewModel>(navController = navController)
                 RegistrationScreen(navController = navController, viewModel = viewModel)
             }
-            composable(route = AppNavConstant.Main.route) {
-                MainScreen(navController = navController)
-            }
-            composable(route = MainNavConstant.Home.route){
-                HomeScreen()
-            }
-            composable(route = MainNavConstant.Profile.route){
-                ProfileScreen()
-            }
-            composable(route = MainNavConstant.Search.route){
-                SearchScreen()
-            }
-            composable(route = MainNavConstant.Add.route){
-                AddScreen()
-            }
         }
-
+        composable(route = AppNavConstant.Main.route) {
+            MainScreen()
+        }
     }
 }
 
