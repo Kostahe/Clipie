e9d3175900939e74e4bf17d756e40332778c17c7
Konstantyn <gagarin2789@gmail.com>
2024-02-04 00:55:39 +0100

    Changed AuthenticationPasswordTextField from custom colors to material colors

--- app/src/main/java/com/clipie/presentation/auth/components/AuthenticationPasswordTextField.kt
+++ app/src/main/java/com/clipie/presentation/auth/components/AuthenticationPasswordTextField.kt
@@ -6,29 +6,21 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.unit.dp
 import com.clipie.R
-import com.clipie.ui.theme.Background
-import com.clipie.ui.theme.CursorTextField
-import com.clipie.ui.theme.FocusedOnTextField
-import com.clipie.ui.theme.TextField
-import com.clipie.ui.theme.UnFocusedOnTextField
 
 @Composable
 fun AuthenticationPasswordTextField(
@@ -40,9 +32,6 @@ fun AuthenticationPasswordTextField(
 ) {
     var showPassword by rememberSaveable {
         mutableStateOf(false)
-    }
-    var focusedColor: Color by remember {
-        mutableStateOf(UnFocusedOnTextField)
     }
     val focusManager = LocalFocusManager.current
 
@@ -57,16 +46,9 @@ fun AuthenticationPasswordTextField(
             )
         },
         colors = TextFieldDefaults.colors(
-            unfocusedContainerColor = TextField,
-            focusedContainerColor = TextField,
-            errorContainerColor = TextField,
-            unfocusedIndicatorColor = Background,
-            focusedIndicatorColor = Background,
-            errorIndicatorColor = Background,
-            cursorColor = CursorTextField,
-            errorCursorColor = CursorTextField,
-            focusedLabelColor = FocusedOnTextField,
-            unfocusedLabelColor = UnFocusedOnTextField
+            unfocusedIndicatorColor = MaterialTheme.colorScheme.surface,
+            focusedIndicatorColor = MaterialTheme.colorScheme.surface,
+            errorIndicatorColor = MaterialTheme.colorScheme.surface,
         ),
         trailingIcon = {
             if (showPassword) {
@@ -74,7 +56,6 @@ fun AuthenticationPasswordTextField(
                     Icon(
                         painter = painterResource(id = R.drawable.icon_visibility_on),
                         contentDescription = stringResource(R.string.password_visible),
-                        tint = focusedColor
                     )
                 }
             } else {
@@ -82,20 +63,11 @@ fun AuthenticationPasswordTextField(
                     Icon(
                         painter = painterResource(id = R.drawable.icon_visibility_off),
                         contentDescription = stringResource(R.string.password_invisible),
-                        tint = focusedColor
                     )
                 }
             }
         },
         shape = RoundedCornerShape(25),
         modifier = modifier
-            .onFocusChanged {
-                focusedColor = if (it.isFocused) {
-                    FocusedOnTextField
-                } else {
-                    UnFocusedOnTextField
-                }
-            }
-            .border(1.dp, focusedColor, RoundedCornerShape(25))
     )
 }
