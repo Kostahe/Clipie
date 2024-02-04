593d5088d3d988acbb6aad6e1e0e877942fd32f4
Konstantyn <gagarin2789@gmail.com>
2024-02-04 18:29:48 +0100

    Updated AuthenticationPasswordTextField

--- app/src/main/java/com/clipie/presentation/auth/components/AuthenticationPasswordTextField.kt
+++ app/src/main/java/com/clipie/presentation/auth/components/AuthenticationPasswordTextField.kt
@@ -1,9 +1,11 @@
 package com.clipie.presentation.auth.components
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -17,9 +19,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import com.clipie.R
 
 @Composable
@@ -30,7 +34,7 @@ fun AuthenticationPasswordTextField(
     imeAction: ImeAction,
     label: String,
 ) {
-    var showPassword by rememberSaveable {
+    var passwordVisible by rememberSaveable {
         mutableStateOf(false)
     }
     val focusManager = LocalFocusManager.current
@@ -38,30 +42,35 @@ fun AuthenticationPasswordTextField(
     TextField(
         value = value,
         onValueChange = onValueChange,
-        keyboardOptions = KeyboardOptions(imeAction = imeAction),
+        keyboardOptions = KeyboardOptions(
+            imeAction = imeAction,
+            keyboardType = KeyboardType.Password
+        ),
+        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
         keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
         label = {
             Text(
                 text = label,
             )
         },
+        singleLine = true,
         colors = TextFieldDefaults.colors(
             unfocusedIndicatorColor = MaterialTheme.colorScheme.surface,
             focusedIndicatorColor = MaterialTheme.colorScheme.surface,
             errorIndicatorColor = MaterialTheme.colorScheme.surface,
         ),
         trailingIcon = {
-            if (showPassword) {
-                IconButton(onClick = { showPassword = false }) {
+            if (passwordVisible) {
+                IconButton(onClick = { passwordVisible = false }) {
                     Icon(
-                        painter = painterResource(id = R.drawable.icon_visibility_on),
+                        imageVector = Icons.Filled.Visibility,
                         contentDescription = stringResource(R.string.password_visible),
                     )
                 }
             } else {
-                IconButton(onClick = { showPassword = true }) {
+                IconButton(onClick = { passwordVisible = true }) {
                     Icon(
-                        painter = painterResource(id = R.drawable.icon_visibility_off),
+                        imageVector = Icons.Filled.VisibilityOff,
                         contentDescription = stringResource(R.string.password_invisible),
                     )
                 }
