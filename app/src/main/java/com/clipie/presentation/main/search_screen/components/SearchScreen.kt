b3a89cc3d08b2d883539dfb665ffa08f53de948d
Toiletman007-PC <quertical5@gmail.com>
2024-02-04 10:10:09 -0800

    Added SearchScreenTopBar

--- app/src/main/java/com/clipie/presentation/main/search_screen/components/SearchScreen.kt
+++ app/src/main/java/com/clipie/presentation/main/search_screen/components/SearchScreen.kt
@@ -1,9 +1,29 @@
 package com.clipie.presentation.main.search_screen.components
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 
 @Composable
 fun SearchScreen(){
-    Text(text = "Search screen")
+
+
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchScreenTopBar () {
+    TopAppBar(title = {
+
+
+
+
+    })
 }
