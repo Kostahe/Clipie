79a9c9e7c34fc2bb0797c39650c8385c7def7179
Toiletman007-PC <quertical5@gmail.com>
2024-02-05 06:37:01 -0800

    Finished SearchScreenTopBar

--- app/src/main/java/com/clipie/presentation/main/search_screen/components/SearchScreen.kt
+++ app/src/main/java/com/clipie/presentation/main/search_screen/components/SearchScreen.kt
@@ -1,16 +1,19 @@
 package com.clipie.presentation.main.search_screen.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.MoreHoriz
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,42 +22,59 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import com.clipie.ui.theme.md_theme_dark_outline
-import com.clipie.ui.theme.md_theme_light_outline
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.clipie.ui.theme.ClipieTheme
 
 @Composable
 fun SearchScreen() {
-
+    Text(text = "Random unordered content")
 
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
+@PreviewLightDark
 @Composable
 fun SearchScreenTopBar() {
-    TopAppBar(title = {
-        var searchText by rememberSaveable { mutableStateOf("") }
+    val customTextFieldColors = TextFieldDefaults.colors(
+        unfocusedIndicatorColor = Color.Transparent
+    )
+    ClipieTheme {
+        TopAppBar(title = {
+            var searchText by rememberSaveable { mutableStateOf("") }
 
-        Icon(imageVector = Icons.AutoMirrored.Outlined.ArrowBack, contentDescription = null)
-        Row(
-            modifier = Modifier.background(
-                shape = RoundedCornerShape(50),
-                color = if (isSystemInDarkTheme()) {
-                    md_theme_dark_outline
-                } else {
-                    md_theme_light_outline
-                }
-            )
-        ) {
-            Icon(imageVector = Icons.Outlined.Search, contentDescription = null)
-            TextField(
+            OutlinedTextField(
                 value = searchText,
-                onValueChange = { searchText = it },
+                enabled = false,
+                onValueChange = { searchText = it; },
                 label = { Text(text = "Search") },
-                singleLine = true
+                colors = customTextFieldColors,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Search,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(30.dp)
+                    )
+                },shape = RoundedCornerShape(20),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .offset(y = (-4).dp)
+                    .clickable { },
             )
-        }
 
-//
 
-    })
+        }, navigationIcon = {},
+            actions = {
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Outlined.MoreHoriz,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(30.dp)
+                    )
+                }
+            }
+        )
+    }
 }
