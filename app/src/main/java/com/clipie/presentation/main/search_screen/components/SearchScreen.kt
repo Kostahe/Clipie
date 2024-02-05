aeddc720322d7e9d80a91cb21e407d3499935044
Toiletman007-PC <quertical5@gmail.com>
2024-02-05 08:19:27 -0800

    Deleted TopAppBar Created SearchBar added a searchHistory

--- app/src/main/java/com/clipie/presentation/main/search_screen/components/SearchScreen.kt
+++ app/src/main/java/com/clipie/presentation/main/search_screen/components/SearchScreen.kt
@@ -1,80 +1,88 @@
 package com.clipie.presentation.main.search_screen.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.MoreHoriz
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.History
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.clipie.ui.theme.ClipieTheme
 
 @Composable
 fun SearchScreen() {
+
     Text(text = "Random unordered content")
 
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
-@PreviewLightDark
 @Composable
 fun SearchScreenTopBar() {
-    val customTextFieldColors = TextFieldDefaults.colors(
-        unfocusedIndicatorColor = Color.Transparent
-    )
+    var searchText by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+    val searchHistory =
+        remember { mutableStateListOf<String>() }
     ClipieTheme {
-        TopAppBar(title = {
-            var searchText by rememberSaveable { mutableStateOf("") }
-
-            OutlinedTextField(
-                value = searchText,
-                enabled = false,
-                onValueChange = { searchText = it; },
-                label = { Text(text = "Search") },
-                colors = customTextFieldColors,
-                leadingIcon = {
+        SearchBar(
+            modifier = Modifier.offset(y = (-5).dp),
+            query = searchText,
+            onQueryChange = { searchText = it },
+            onSearch = { active = false; searchHistory.add(it) },
+            active = active,
+            onActiveChange = { active = it },
+            placeholder = { Text(text = "Search") },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.Search,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(30.dp)
+                )
+            }, trailingIcon = {
+                if (active) {
                     Icon(
-                        imageVector = Icons.Outlined.Search,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(30.dp)
-                    )
-                },shape = RoundedCornerShape(20),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .offset(y = (-4).dp)
-                    .clickable { },
-            )
-
-
-        }, navigationIcon = {},
-            actions = {
-                IconButton(onClick = { }) {
-                    Icon(
-                        imageVector = Icons.Outlined.MoreHoriz,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(30.dp)
+                        modifier = Modifier.clickable {
+                            if (searchText.isNotEmpty()) {
+                                searchText = ""
+                            } else {
+                                active = false
+                            }
+                        },
+                        imageVector = Icons.Outlined.Close,
+                        contentDescription = null
                     )
                 }
             }
-        )
+
+        ) {
+            searchHistory.forEach {
+
+                Row(modifier = Modifier.padding(all = 14.dp)) {
+                    Icon(
+                        imageVector = Icons.Outlined.History,
+                        contentDescription = null,
+                        modifier = Modifier.padding(end = 15.dp)
+                    )
+                    Text(text = it)
+                }
+
+            }
+        }
     }
+
 }
