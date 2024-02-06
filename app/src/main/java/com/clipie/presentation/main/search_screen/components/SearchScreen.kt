ab656ad56e1950a451af9838a349bf169c550a62
Toiletman007-PC <quertical5@gmail.com>
2024-02-06 02:40:21 -0800

    made the list scrollable

--- app/src/main/java/com/clipie/presentation/main/search_screen/components/SearchScreen.kt
+++ app/src/main/java/com/clipie/presentation/main/search_screen/components/SearchScreen.kt
@@ -1,10 +1,16 @@
 package com.clipie.presentation.main.search_screen.components
 
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.History
@@ -70,18 +76,30 @@ fun SearchScreenTopBar() {
             }
 
         ) {
-            searchHistory.forEach {
+            
+            LazyColumn {
+                items(searchHistory){
+                    Row(modifier = Modifier
+                        .padding(all = 14.dp)
+                        .clickable { searchText = it }) {
 
-                Row(modifier = Modifier.padding(all = 14.dp)) {
-                    Icon(
-                        imageVector = Icons.Outlined.History,
-                        contentDescription = null,
-                        modifier = Modifier.padding(end = 15.dp)
-                    )
-                    Text(text = it)
+                        Icon(
+                            imageVector = Icons.Outlined.History,
+                            contentDescription = null,
+                            modifier = Modifier.padding(end = 15.dp)
+                        )
+
+                        Text(text = it)
+                        Spacer(modifier = Modifier.weight(1f))
+                        Icon(imageVector = Icons.Outlined.Close,
+                            contentDescription = null,
+                            modifier = Modifier.clickable { searchHistory.remove(it) })
+                    }
                 }
 
             }
+
+
         }
     }
 
