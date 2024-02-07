601b3b2d2f2f4dda6a31dd131d9f78ceade516ad
Toiletman007-PC <quertical5@gmail.com>
2024-02-07 17:28:04 +0100

    Fix the MoreDropdownMenu location and started work on MoreDropdownMenuItem

--- app/src/main/java/com/clipie/presentation/main/search_screen/components/SearchScreen.kt
+++ app/src/main/java/com/clipie/presentation/main/search_screen/components/SearchScreen.kt
@@ -2,12 +2,16 @@ package com.clipie.presentation.main.search_screen.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -20,6 +24,9 @@ import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,7 +40,6 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Popup
 import com.clipie.ui.theme.ClipieTheme
 
 @Composable
@@ -52,7 +58,7 @@ fun SearchScreenTopBar() {
     var moreMenu by rememberSaveable { mutableStateOf(false) }
     var selectSwitch by rememberSaveable { mutableStateOf(false) }
     ClipieTheme {
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+        Row(modifier = Modifier.fillMaxWidth()) {
 
             SearchBar(
                 modifier = Modifier.offset(y = (-5).dp),
@@ -101,27 +107,28 @@ fun SearchScreenTopBar() {
                     }
                 }
             }
-            Icon(imageVector = Icons.Outlined.MoreHoriz,
-                contentDescription = null, modifier = Modifier
-                    .size(30.dp)
-                    .align(Alignment.CenterVertically)
-                    .clickable { moreMenu = true }
-            )
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Outlined.MoreHoriz,
+                    contentDescription = null, modifier = Modifier
+                        .size(30.dp)
+                        .align(Alignment.CenterVertically)
+                        .clickable { moreMenu = true }
+                )
+                DropdownMenu(expanded = moreMenu,
+                    onDismissRequest = { moreMenu = false }) {
+                    Text(text = "Filter suggestions", modifier = Modifier.padding(horizontal = 10.dp))
+                    MoreDropdownMenuItem("For you", selected = selectSwitch) {
+                        selectSwitch = true
+                    }
+                    MoreDropdownMenuItem("Not Personalised", selected = !selectSwitch) {
+                        selectSwitch = false
+                    }
+                }
+            }
+
 
 
         }
-
-        DropdownMenu(expanded = moreMenu, onDismissRequest = { moreMenu = false }) {
-            Text(text = "Filter suggestions", modifier = Modifier.padding(horizontal = 10.dp))
-            MoreDropdownMenuItem(selected = selectSwitch) {
-                selectSwitch = true
-            }
-            MoreDropdownMenuItem(selected = !selectSwitch) {
-                selectSwitch = false
-            }
-        }
-
-
     }
 }
 
@@ -157,13 +164,22 @@ fun SearchHistoryItem(
 
 
 @Composable
-fun MoreDropdownMenuItem(selected: Boolean, onClick: () -> Unit) {
+fun MoreDropdownMenuItem(text: String, selected: Boolean, onClick: () -> Unit) {
 
     DropdownMenuItem(text = {
-        if (selected) {
-            Icon(imageVector = Icons.Outlined.Check, contentDescription = null)
-
+        Row(modifier = Modifier.height(50.dp)) {
+            if (selected) {
+                Box(modifier = Modifier.size(30.dp)){
+                    Icon(imageVector = Icons.Outlined.Check,
+                        contentDescription = null,
+                        modifier = Modifier.size(25.dp))
+                }
+            }else{
+                Spacer(modifier = Modifier.width(30.dp))
+            }
+            Text(text = text, style = MaterialTheme.typography.titleLarge)
         }
+
 
 
     }, onClick = { onClick() })
