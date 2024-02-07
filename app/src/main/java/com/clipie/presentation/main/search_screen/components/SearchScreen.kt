e67f6deb49bdb212e25e700d5815a822495f3c7b
Toiletman007-PC <quertical5@gmail.com>
2024-02-07 16:06:54 +0100

    Added a row to the topbar made all items spaceAround and added a clickable icon

--- app/src/main/java/com/clipie/presentation/main/search_screen/components/SearchScreen.kt
+++ app/src/main/java/com/clipie/presentation/main/search_screen/components/SearchScreen.kt
@@ -1,8 +1,10 @@
 package com.clipie.presentation.main.search_screen.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -11,19 +13,20 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.History
+import androidx.compose.material.icons.outlined.MoreHoriz
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.clipie.ui.theme.ClipieTheme
@@ -42,53 +45,62 @@ fun SearchScreenTopBar() {
     var active by remember { mutableStateOf(false) }
     val searchHistory = remember { mutableStateListOf<String>() }
     ClipieTheme {
-        SearchBar(
-            modifier = Modifier.offset(y = (-5).dp),
-            query = searchText,
-            onQueryChange = { searchText = it },
-            onSearch = {
-                active = false; if (searchText.isNotEmpty()) {
-                searchHistory.add(it)
-            }
-            },
-            active = active,
-            onActiveChange = { active = it },
-            placeholder = { Text(text = "Search") },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Outlined.Search,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(30.dp)
-                )
-            }, trailingIcon = {
-                if (active) {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+
+            SearchBar(
+                modifier = Modifier.offset(y = (-5).dp),
+                query = searchText,
+                onQueryChange = { searchText = it },
+                onSearch = {
+                    active = false; if (searchText.isNotEmpty()) {
+                    searchHistory.add(it)
+                    }
+                },
+                active = active,
+                onActiveChange = { active = it },
+                placeholder = { Text(text = "Search") },
+                leadingIcon = {
                     Icon(
-                        modifier = Modifier.clickable {
-                            if (searchText.isNotEmpty()) {
-                                searchText = ""
-                            } else {
-                                active = false
-                            }
-                        },
-                        imageVector = Icons.Outlined.Close,
-                        contentDescription = null
+                        imageVector = Icons.Outlined.Search,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(30.dp)
                     )
+                }, trailingIcon = {
+                    if (active) {
+                        Icon(
+                            modifier = Modifier.clickable {
+                                if (searchText.isNotEmpty()) {
+                                    searchText = ""
+                                } else {
+                                    active = false
+                                }
+                            },
+                            imageVector = Icons.Outlined.Close,
+                            contentDescription = null
+                        )
+                    }
+                }
+
+            ) {
+
+                LazyColumn {
+                    items(searchHistory) {
+                        SearchHistoryItem(
+                            searchHistoryList = searchHistory,
+                            it = it,
+                            searchText = searchText
+                        )
+                    }
                 }
             }
-
-        ) {
-
-            LazyColumn {
-                items(searchHistory) {
-                    SearchHistoryItem(
-                        searchHistoryList = searchHistory,
-                        it = it,
-                        searchText = searchText
-                    )
-                }
-            }
+            Icon(imageVector = Icons.Outlined.MoreHoriz,
+                contentDescription = null, modifier = Modifier.size(30.dp)
+                    .align(Alignment.CenterVertically)
+                    .clickable {  }
+            )
         }
+
     }
 }
 
@@ -101,6 +113,7 @@ fun SearchHistoryItem(
 ) {
     var searchText = searchText
     Row(modifier = Modifier
+        .fillMaxWidth(1f)
         .padding(all = 14.dp)
         .clickable { searchText = it }) {
 
