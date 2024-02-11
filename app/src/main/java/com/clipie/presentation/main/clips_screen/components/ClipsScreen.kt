ad15605f4f085c6e138c5dc05c35bdbe176f6af1
nooooooootbuk <quertical5@gmail.com>
2024-02-11 16:33:15 +0100

    Changed the fontweight

--- app/src/main/java/com/clipie/presentation/main/clips_screen/components/ClipsScreen.kt
+++ app/src/main/java/com/clipie/presentation/main/clips_screen/components/ClipsScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.clipie.ui.theme.ClipieTheme
 
@@ -35,7 +36,7 @@ fun ClipsScreen() {
 @Composable
 fun ClipsTopAppBar(title: String, backgroundColor: Color = Color.Transparent) {
     TopAppBar(
-        title = { Text(text = "Clips", style = MaterialTheme.typography.headlineMedium) },
+        title = { Text(text = "Clips", style = MaterialTheme.typography.headlineMedium, fontWeight = FontWeight.Bold) },
         modifier = Modifier
             .fillMaxWidth(1f)
             .background(backgroundColor),
