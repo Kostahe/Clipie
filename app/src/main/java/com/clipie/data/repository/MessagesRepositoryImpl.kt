5df4deb8d9df84cf4a56cb5d89ee248ca32e1a9a
Kostahe <huzilkon@gmail.com>
2024-03-07 09:52:04 +0100

    Created messages repository

---
+++ app/src/main/java/com/clipie/data/repository/MessagesRepositoryImpl.kt
@@ -0,0 +1,16 @@
+package com.clipie.data.repository
+
+import com.clipie.data.common.FireStoreTable
+import com.clipie.domain.entities.State
+import com.clipie.domain.model.Message
+import com.clipie.domain.repository.MessagesRepository
+import com.google.firebase.firestore.FirebaseFirestore
+import javax.inject.Inject
+
+class MessagesRepositoryImpl @Inject constructor(
+    private val fireStore: FirebaseFirestore
+): MessagesRepository {
+    override fun sendMessage(message: Message, result: (State<Unit>) -> Unit) {
+        fireStore.collection(FireStoreTable.MESSAGE.tableName).add(message)
+    }
+}
