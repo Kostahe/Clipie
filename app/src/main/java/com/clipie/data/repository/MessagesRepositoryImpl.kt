c50c896bd8c952087e7fe81470e8038bf36d840f
Kostahe <huzilkon@gmail.com>
2024-03-07 10:21:25 +0100

    Updated function sendMessage in MessagesRepositoryImpl

--- app/src/main/java/com/clipie/data/repository/MessagesRepositoryImpl.kt
+++ app/src/main/java/com/clipie/data/repository/MessagesRepositoryImpl.kt
@@ -11,6 +11,15 @@ class MessagesRepositoryImpl @Inject constructor(
     private val fireStore: FirebaseFirestore
 ): MessagesRepository {
     override fun sendMessage(message: Message, result: (State<Unit>) -> Unit) {
-        fireStore.collection(FireStoreTable.MESSAGE.tableName).add(message)
+        fireStore.collection(FireStoreTable.MESSAGE.tableName)
+            .add(message)
+            .addOnSuccessListener { documentRef ->
+                message.id = documentRef.id
+                documentRef.set(message)
+                result.invoke(State.Success(Unit))
+            }
+            .addOnFailureListener { exception->
+                result.invoke(State.Error(exception.message ?: ""))
+            }
     }
 }
