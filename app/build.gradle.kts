961af2651f3eafc6c4f0fdca8f3a6091e69fcd83
Toiletman007-PC <quertical5@gmail.com>
2024-03-11 21:44:29 +0100

    Implementation for camerax

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,4 +98,14 @@ dependencies {
     // Coil
     val coilVersion = "2.5.0"
     implementation("io.coil-kt:coil-compose:$coilVersion")
+    // Camerax
+    val cameraxVersion = "1.3.0-rc01"
+
+    implementation("androidx.camera:camera-core:$cameraxVersion")
+    implementation("androidx.camera:camera-camera2:$cameraxVersion")
+    implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
+    implementation("androidx.camera:camera-video:$cameraxVersion")
+
+    implementation("androidx.camera:camera-view:$cameraxVersion")
+    implementation("androidx.camera:camera-extensions:$cameraxVersion")
 }
