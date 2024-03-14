f3cf70dfbddef4a436623f36948a6e5f86699d3d
Toiletman007 <quertical5@gmail.com>
2024-03-14 10:28:19 +0100

    Finished CameraPreview

---
+++ app/src/main/java/com/clipie/main/presentation/add/camera/CameraPreview.kt
@@ -0,0 +1,28 @@
+package com.clipie.main.presentation.add.camera
+
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun CameraPreview(
+    modifier: Modifier = Modifier,
+    controller: LifecycleCameraController
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    AndroidView(
+        factory = {
+            PreviewView(it).apply {
+//                Add more functionality if needed!
+                this.controller = controller
+                controller.bindToLifecycle(lifecycleOwner)
+            }
+        },
+        update = {},
+        modifier = modifier
+    )
+
+}
