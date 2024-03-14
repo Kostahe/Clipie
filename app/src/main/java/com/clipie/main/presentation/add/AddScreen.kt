5fd23b1d6c756b6e194144f2f7bbb14f9688a1ef
Toiletman007 <quertical5@gmail.com>
2024-03-14 10:34:21 +0100

    Added the camera for now

--- app/src/main/java/com/clipie/main/presentation/add/AddScreen.kt
+++ app/src/main/java/com/clipie/main/presentation/add/AddScreen.kt
@@ -2,8 +2,11 @@ package com.clipie.main.presentation.add
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.clipie.main.presentation.add.camera.CameraPreview
+import com.clipie.main.presentation.add.camera.CameraScreen
 
 @Composable
 fun AddScreen(){
     Text(text = "Navigate to CameraScreen")
+    CameraScreen()
 }
