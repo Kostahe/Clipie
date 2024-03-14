ada26ab691efd59f36fd2adffa6f328b8ca44d83
Toiletman007 <quertical5@gmail.com>
2024-03-14 10:28:49 +0100

    Added and made a CameraPreview (working)

--- app/src/main/java/com/clipie/main/presentation/add/camera/CameraScreen.kt
+++ app/src/main/java/com/clipie/main/presentation/add/camera/CameraScreen.kt
@@ -3,7 +3,12 @@ package com.clipie.main.presentation.add.camera
 import android.Manifest
 import android.app.Activity
 import android.content.pm.PackageManager
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -16,6 +21,14 @@ fun CameraScreen() {
         Manifest.permission.RECORD_AUDIO
     )
     val context = LocalContext.current
+    val controller = remember {
+        LifecycleCameraController(context).apply {
+            setEnabledUseCases(
+                CameraController.IMAGE_CAPTURE or
+                CameraController.VIDEO_CAPTURE
+            )
+        }
+    }
     fun hasRequiredPermissions():Boolean{
         return cameraxPermissions.all {
             ContextCompat.checkSelfPermission(
@@ -30,6 +43,7 @@ fun CameraScreen() {
         val activity: Activity = context as Activity
         ActivityCompat.requestPermissions(activity, cameraxPermissions, 0)
     }
+    CameraPreview(controller =controller, modifier = Modifier.fillMaxSize())
 
 
 
