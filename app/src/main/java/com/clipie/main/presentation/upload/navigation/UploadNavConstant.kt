6f00cd3aebce02f81b2419687d327ea0b81e24ce
Toiletman007-PC <quertical5@gmail.com>
2024-03-16 20:17:35 +0100

    Updated and filled enum class

--- app/src/main/java/com/clipie/main/presentation/upload/navigation/UploadNavConstant.kt
+++ app/src/main/java/com/clipie/main/presentation/upload/navigation/UploadNavConstant.kt
@@ -1,4 +1,10 @@
 package com.clipie.main.presentation.upload.navigation
 
-class UploadNavConstant {
+import android.graphics.drawable.Icon
+
+enum class UploadNavConstant(val route: String) {
+    Post("post"),
+    Story("story"),
+    Clip("clip"),
+    Live("live")
 }
