a7e2801b779978a33620c8ebea8abe2338f6c146
Toiletman007-PC <quertical5@gmail.com>
2024-03-16 20:17:50 +0100

    added NavHost

--- app/src/main/java/com/clipie/main/presentation/upload/navigation/UploadNavHost.kt
+++ app/src/main/java/com/clipie/main/presentation/upload/navigation/UploadNavHost.kt
@@ -1,2 +1,33 @@
 package com.clipie.main.presentation.upload.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun UploadNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController
+) {
+    NavHost(navController = navController,
+        startDestination = UploadNavConstant.Post.route,
+        modifier = modifier
+    ){
+        composable(route = UploadNavConstant.Post.route){
+
+        }
+        composable(route = UploadNavConstant.Story.route){
+
+        }
+        composable(route = UploadNavConstant.Clip.route){
+
+        }
+        composable(route = UploadNavConstant.Live.route){
+
+        }
+
+    }
+
+}
