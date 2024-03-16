adf90c81b3a3cbd627801deb2250456e333dd3af
Toiletman007-PC <quertical5@gmail.com>
2024-03-16 18:14:59 +0100

    Added a close button

--- app/src/main/java/com/clipie/main/presentation/add/AddScreen.kt
+++ app/src/main/java/com/clipie/main/presentation/add/AddScreen.kt
@@ -1,5 +1,9 @@
 package com.clipie.main.presentation.add
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import com.clipie.main.presentation.add.camera.CameraPreview
@@ -7,6 +11,9 @@ import com.clipie.main.presentation.add.camera.CameraScreen
 
 @Composable
 fun AddScreen(){
-    Text(text = "Navigate to CameraScreen")
-    CameraScreen()
+    Text(text = "Navigate to NewPostScreen")
+    IconButton(onClick = { /*TODO Navigate to HomeScreen*/ }) {
+        Icon(imageVector = Icons.Default.Close, contentDescription = null)
+    }
+//    CameraScreen()
 }
