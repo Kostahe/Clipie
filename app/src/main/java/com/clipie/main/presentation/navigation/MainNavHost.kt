5875d1f45720ef1df42f52968bdfe118c4bef6df
Toiletman007-PC <quertical5@gmail.com>
2024-03-16 20:19:06 +0100

    changed names for Upload screen

--- app/src/main/java/com/clipie/main/presentation/navigation/MainNavHost.kt
+++ app/src/main/java/com/clipie/main/presentation/navigation/MainNavHost.kt
@@ -5,11 +5,11 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.clipie.main.presentation.add.AddScreen
 import com.clipie.main.presentation.clips.ClipsScreen
 import com.clipie.main.presentation.home.HomeScreen
 import com.clipie.main.presentation.profile.ProfileScreen
 import com.clipie.main.presentation.search.SearchScreen
+import com.clipie.main.presentation.upload.UploadScreen
 
 @Composable
 fun MainNavHost(
@@ -28,7 +28,7 @@ fun MainNavHost(
             SearchScreen()
         }
         composable(route = MainNavConstant.Add.route){
-            AddScreen(navController)
+            UploadScreen(navController)
         }
         composable(route = MainNavConstant.Clips.route){
             ClipsScreen()
