9d7b510f5ebf341054a091adc5a4337f3b5c098a
Toiletman007 <quertical5@gmail.com>
2024-03-18 11:28:09 +0100

    Finished TopAppBar

--- app/src/main/java/com/clipie/main/presentation/upload/screens/UploadPostScreen.kt
+++ app/src/main/java/com/clipie/main/presentation/upload/screens/UploadPostScreen.kt
@@ -2,15 +2,28 @@ package com.clipie.main.presentation.upload.screens
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.clipie.main.presentation.navigation.MainNavConstant
 import com.clipie.main.presentation.upload.components.BackHomeButton
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UploadPostScreen(navController: NavController) {
-    BackHomeButton(navController = navController)
-
+    TopAppBar(title = {
+        Text(text = "New Post", fontWeight = FontWeight.Bold) },
+        navigationIcon = {BackHomeButton(navController = navController)}, actions = {
+            TextButton(onClick = { /*TODO Move to the next screen*/ }) {
+                Text(text = "Next", modifier = Modifier, fontSize = 25.sp)
+            }
+        })
 }
