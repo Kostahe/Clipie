86787ed159495b29aaa0c00c593941cdaff6952f
Toiletman007-PC <quertical5@gmail.com>
2024-03-18 18:28:20 +0100

    Changed names

--- app/src/main/java/com/clipie/main/presentation/MainScreen.kt
+++ app/src/main/java/com/clipie/main/presentation/MainScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -73,7 +74,7 @@ fun MainScreen(
             if (currentRoute == MainNavConstant.Upload.route){
 //              Don't forget to add a Bottom bar here for Add screen with
 //              recycler view (Post, Story, Clip, Live)
-                CameraBottomBar(navController)
+                UploadBottomBar(navController)
             }else{
                 MainBottomBar(navController)
             }
@@ -136,6 +137,6 @@ fun MainBottomBar(navController: NavHostController) {
 }
 
 @Composable
-fun CameraBottomBar(navController: NavHostController) {
-
+fun UploadBottomBar(navController: NavHostController) {
+    Text(text = "Test")
 }
