29c8a7102f86b97db0cf0927a59a09c23cc6ff9d
Toiletman007 <quertical5@gmail.com>
2024-03-18 11:38:48 +0100

    Text added

--- app/src/main/java/com/clipie/main/presentation/upload/screens/UploadStoryScreen.kt
+++ app/src/main/java/com/clipie/main/presentation/upload/screens/UploadStoryScreen.kt
@@ -1,11 +1,18 @@
 package com.clipie.main.presentation.upload.screens
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.clipie.main.presentation.upload.components.BackHomeButton
 
 @Composable
 fun UploadStoryScreen(navController: NavController) {
-    BackHomeButton(navController = navController)
+    Row(modifier = Modifier.fillMaxWidth()) {
+        BackHomeButton(navController = navController)
+        Text(text = "New Story", modifier = Modifier.weight(1f))
+    }
 
 }
