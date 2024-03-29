8eb42aa8673e228aa5674744213246261938689f
Toiletman007-PC <quertical5@gmail.com>
2024-03-30 00:11:41 +0100

    Made it into a separate file

--- app/src/main/java/com/clipie/main/presentation/upload/screens/UploadPostScreen.kt
+++ app/src/main/java/com/clipie/main/presentation/upload/screens/UploadPostScreen.kt
@@ -1,33 +1,24 @@
 package com.clipie.main.presentation.upload.screens
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import com.clipie.main.presentation.navigation.MainNavConstant
-import com.clipie.main.presentation.upload.components.BackHomeButton
-import com.clipie.main.presentation.upload.navigation.UploadNavConstant
 
-@OptIn(ExperimentalMaterial3Api::class)
+import androidx.navigation.NavController
+
 @Composable
 fun UploadPostScreen(navController: NavController) {
-    TopAppBar(title = {
-        Text(text = "New Post", fontWeight = FontWeight.Bold)
-    },
-        navigationIcon = { BackHomeButton(navController = navController) },
-        actions = {
-            TextButton(onClick = { navController.navigate(UploadNavConstant.UploadClip.route) }) {
-                Text(text = "Next", modifier = Modifier, fontSize = 25.sp)
+
+    Text(text = "Upload Post Screen")
+
+        LazyRow() {
+            items(20) { index ->
+                Text(text = "test$index")
             }
-        })
+
+        }
+
+
+
+
 }
