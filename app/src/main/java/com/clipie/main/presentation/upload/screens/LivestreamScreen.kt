8f026b86f420cdb853ff260cbd4d0242305324a1
Toiletman007-PC <quertical5@gmail.com>
2024-03-30 00:09:52 +0100

    Created a transparent topbar

--- app/src/main/java/com/clipie/main/presentation/upload/screens/LivestreamScreen.kt
+++ app/src/main/java/com/clipie/main/presentation/upload/screens/LivestreamScreen.kt
@@ -1,13 +1,43 @@
 package com.clipie.main.presentation.upload.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.clipie.camera.CameraScreen
 import com.clipie.main.presentation.upload.components.BackHomeButton
 
 @Composable
 fun LivestreamScreen(navController: NavController) {
-    BackHomeButton(navController = navController)
-    Text(text = "Start Livestream")
 
+    Box(modifier = Modifier.fillMaxSize()){
+        CameraScreen(modifier = Modifier)
+        Row(modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.SpaceBetween, ) {
+            BackHomeButton(navController = navController)
+
+            Text(text = "<Active follower count>")
+
+            IconButton(onClick = { /*TODO*/ }) {
+
+                Icon(imageVector = Icons.Filled.Settings, contentDescription = null)
+            }
+        }
+    }
 }
