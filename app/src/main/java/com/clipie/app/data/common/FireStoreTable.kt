8d5feb02f348b181c85cfdb6cc3c002ffded591d
Konstantyn <gagarin2789@gmail.com>
2024-03-30 00:47:42 +0100

    Added new tables to FireStoreTable enum

--- app/src/main/java/com/clipie/app/data/common/FireStoreTable.kt
+++ app/src/main/java/com/clipie/app/data/common/FireStoreTable.kt
@@ -2,5 +2,6 @@ package com.clipie.app.data.common
 
 enum class FireStoreTable(val tableName: String) {
     USER("user"),
-    MESSAGE("message")
+    MESSAGE("message"),
+    CHAT("chat")
 }
--- app/src/main/java/com/clipie/ui/theme/Theme.kt
+++ app/src/main/java/com/clipie/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.clipie.app.presentation.ui.theme
+package com.clipie.ui.theme
 
 import android.app.Activity
 import android.os.Build
