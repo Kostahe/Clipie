bbd910b7a8c6e26febd370611c4317af8a53337b
Konstantyn <gagarin2789@gmail.com>
2024-03-30 01:58:26 +0100

    Resolved conflicts

--- app/src/main/java/com/clipie/main/data/repository/MessagesRepositoryImpl.kt
+++ app/src/main/java/com/clipie/main/data/repository/MessagesRepositoryImpl.kt
@@ -1,78 +1,55 @@
 package com.clipie.main.data.repository
 
-import com.clipie.app.data.common.FireStoreTable
-import com.clipie.app.domain.entities.State
+
 import com.clipie.authentication.domain.models.User
 import com.clipie.main.domain.model.Chat
 import com.clipie.util.FireStoreTable
 import com.clipie.util.Resource
 import com.clipie.main.domain.model.Message
 import com.clipie.main.domain.repository.MessagesRepository
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import javax.inject.Inject
 
 class MessagesRepositoryImpl @Inject constructor(
-    private val fireStore: FirebaseFirestore
+    private val firebaseAuth: FirebaseAuth, private val fireStore: FirebaseFirestore
 ) : MessagesRepository {
 
-
-    override fun getChatListFromCurrentUser(result: (State<List<Chat>>) -> Unit) {
+    override fun getChatListFromCurrentUser(result: (Resource<List<Chat>>) -> Unit) {
         fireStore.collection(FireStoreTable.CHAT.tableName)
-            .whereArrayContains("usersId", authentication.currentUser?.uid.orEmpty())
-            .get()
+            .whereArrayContains("usersId", firebaseAuth.currentUser?.uid.orEmpty()).get()
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     val chatList: List<Chat> = task.result.toObjects(Chat::class.java)
-                    result.invoke(State.Success(chatList))
+                    result.invoke(Resource.Success(chatList))
                 } else {
-                    result.invoke(State.Error(task.exception?.message.orEmpty()))
+                    result.invoke(Resource.Error(task.exception?.message.orEmpty()))
                 }
             }
     }
 
-    override fun getMessagesFromChat(chat: Chat, result: (State<List<Message>>) -> Unit) {
-        fireStore.collection(FireStoreTable.MESSAGE.tableName)
-            .whereEqualTo("chatId", chat.id)
-            .get()
+    override fun getMessagesFromChat(chat: Chat, result: (Resource<List<Message>>) -> Unit) {
+        fireStore.collection(FireStoreTable.MESSAGE.tableName).whereEqualTo("chatId", chat.id).get()
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     val messageList: List<Message> = task.result.toObjects(Message::class.java)
-                    result.invoke(State.Success(messageList))
+                    result.invoke(Resource.Success(messageList))
                 } else {
-                    result.invoke(State.Error(task.exception?.message.orEmpty()))
+                    result.invoke(Resource.Error(task.exception?.message.orEmpty()))
                 }
             }
     }
 
-    override fun sendMessage(message: Message, result: (State<Unit>) -> Unit) {
-): MessagesRepository {
-    override fun sendMessage(message: Message, result: (Resource<Unit>) -> Unit) {
-        fireStore.collection(FireStoreTable.MESSAGE.tableName)
-            .add(message)
-            .addOnSuccessListener { documentRef ->
-                message.id = documentRef.id
-                documentRef.set(message)
-                result.invoke(Resource.Success(Unit))
-            }
-            .addOnFailureListener { exception ->
-                result.invoke(State.Error(exception.message ?: ""))
-            .addOnFailureListener { exception->
-                result.invoke(Resource.Error(exception.message ?: ""))
-            }
-    }
 
-    override fun getUsersFromChat(chat: Chat, result: (State<List<User>>) -> Unit) {
-        fireStore.collection(FireStoreTable.USER.tableName)
-            .whereIn("id", chat.usersId)
-            .get()
+    override fun getUsersFromChat(chat: Chat, result: (Resource<List<User>>) -> Unit) {
+        fireStore.collection(FireStoreTable.USER.tableName).whereIn("id", chat.usersId).get()
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     val userList = task.result?.toObjects(User::class.java)
-                    result.invoke(State.Success(userList))
+                    result.invoke(Resource.Success(userList))
                 } else {
-                    result.invoke(State.Error(task.exception?.message.orEmpty()))
+                    result.invoke(Resource.Error(task.exception?.message.orEmpty()))
                 }
-
             }
     }
 }
--- app/src/main/java/com/clipie/main/domain/repository/MessagesRepository.kt
+++ app/src/main/java/com/clipie/main/domain/repository/MessagesRepository.kt
@@ -1,13 +1,12 @@
 package com.clipie.main.domain.repository
 
-import com.clipie.app.domain.entities.State
 import com.clipie.authentication.domain.models.User
 import com.clipie.main.domain.model.Chat
 import com.clipie.main.domain.model.Message
+import com.clipie.util.Resource
 
 interface MessagesRepository {
-    fun getChatListFromCurrentUser(result: (State<List<Chat>>) -> Unit)
-    fun getMessagesFromChat(chat: Chat, result: (State<List<Message>>) -> Unit)
-    fun sendMessage(message: Message, result: (State<Unit>) -> Unit)
-    fun getUsersFromChat(chat: Chat, result: (State<List<User>>) -> Unit)
+    fun getChatListFromCurrentUser(result: (Resource<List<Chat>>) -> Unit)
+    fun getMessagesFromChat(chat: Chat, result: (Resource<List<Message>>) -> Unit)
+    fun getUsersFromChat(chat: Chat, result: (Resource<List<User>>) -> Unit)
 }
