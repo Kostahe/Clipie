8cfae6d9e33d0c4fb48a6b2ae6465c59df309841
Toiletman007 <quertical5@gmail.com>
2024-04-08 15:43:52 +0200

    formated

--- app/src/main/java/com/clipie/main/presentation/upload/screens/UploadPostScreen.kt
+++ app/src/main/java/com/clipie/main/presentation/upload/screens/UploadPostScreen.kt
@@ -1,20 +1,14 @@
 package com.clipie.main.presentation.upload.screens
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.clipie.main.presentation.navigation.MainNavConstant
 import com.clipie.main.presentation.upload.components.BackHomeButton
 import com.clipie.main.presentation.upload.navigation.UploadNavConstant
 
@@ -22,7 +16,10 @@ import com.clipie.main.presentation.upload.navigation.UploadNavConstant
 @Composable
 fun UploadPostScreen(navController: NavController) {
     TopAppBar(title = {
-        Text(text = "New Post", fontWeight = FontWeight.Bold)
+        Text(
+            text = "New Post",
+            fontWeight = FontWeight.Bold
+        )
     },
         navigationIcon = { BackHomeButton(navController = navController) },
         actions = {
