7a8afae867cad6fac4c962e2baaefc0cdc7185e8
Konstantyn <gagarin2789@gmail.com>
2024-04-28 22:52:07 +0200

    changed field usersId to userList

--- app/src/main/java/com/clipie/main/domain/model/Chat.kt
+++ app/src/main/java/com/clipie/main/domain/model/Chat.kt
@@ -1,8 +1,10 @@
 package com.clipie.main.domain.model
 
+import com.clipie.authentication.domain.models.User
+
 data class Chat(
     var id: String = "",
     val name: String = "",
-    val usersId: List<String> = listOf(),
+    val userList: List<User> = listOf(),
     val lastMessageId: String = ""
 )
