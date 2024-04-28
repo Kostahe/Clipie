475a3309652cfff5ccc2ecb604ea1ccec3ad884b
Konstantyn <gagarin2789@gmail.com>
2024-04-28 22:53:11 +0200

    Binded ChatRepository to hilt

--- app/src/main/java/com/clipie/di/RepositoryModule.kt
+++ app/src/main/java/com/clipie/di/RepositoryModule.kt
@@ -1,9 +1,9 @@
 package com.clipie.di
 
 import com.clipie.authentication.data.repository.AuthenticationRepositoryImpl
-import com.clipie.main.data.repository.MessagesRepositoryImpl
 import com.clipie.authentication.domain.repository.AuthenticationRepository
-import com.clipie.main.domain.repository.MessagesRepository
+import com.clipie.main.data.repository.ChatRepositoryImpl
+import com.clipie.main.domain.repository.ChatRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -20,5 +20,5 @@ interface RepositoryModule {
 
     @Binds
     @Singleton
-    fun bindMessagesRepository(messagesRepositoryImpl: MessagesRepositoryImpl): MessagesRepository
+    fun bindChatRepository(messagesRepositoryImpl: ChatRepositoryImpl): ChatRepository
 }
