93deb4f88a43c3f4ae031b266f360511a64415d2
Kostahe <huzilkon@gmail.com>
2024-05-16 19:28:41 +0200

    Added new Route

--- app/src/main/java/com/clipie/main/presentation/navigation/MainNavHost.kt
+++ app/src/main/java/com/clipie/main/presentation/navigation/MainNavHost.kt
@@ -1,11 +1,16 @@
 package com.clipie.main.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.clipie.main.presentation.chat.ChatScreen
+import com.clipie.main.presentation.chat.ChatViewModel
+import com.clipie.main.presentation.chat.navigation.ChatNavConstant
 import com.clipie.main.presentation.clips.ClipsScreen
 import com.clipie.main.presentation.home.HomeScreen
 import com.clipie.main.presentation.profile.ProfileScreen
@@ -23,18 +28,31 @@ fun MainNavHost(
 ) {
     NavHost(
         navController = navController,
-        startDestination = MainNavConstant.Home.route,
+        startDestination = MainNavConstant.HomeNavigation.route,
         modifier = modifier
     ) {
-        composable(route = MainNavConstant.Home.route) {
-            HomeScreen()
+        navigation(
+            startDestination = MainNavConstant.Home.route,
+            route = MainNavConstant.HomeNavigation.route
+        ) {
+            composable(MainNavConstant.Home.route) {
+                HomeScreen()
+            }
+            composable(ChatNavConstant.Chat.route) {
+                val viewModel = hiltViewModel<ChatViewModel>()
+
+                ChatScreen(chatState = viewModel.state.collectAsState().value)
+            }
         }
         composable(route = MainNavConstant.Search.route){
             SearchScreen()
         }
-        navigation(startDestination = UploadNavConstant.UploadPost.route, route = MainNavConstant.Upload.route){
+        navigation(
+            startDestination = UploadNavConstant.UploadPost.route,
+            route = MainNavConstant.Upload.route
+        ) {
             composable(route = UploadNavConstant.UploadPost.route){
-                UploadPostScreen(navController)
+                UploadPostScreen(navController = navController)
             }
             composable(route = UploadNavConstant.UploadStory.route){
                 UploadStoryScreen(navController = navController)
