68010e19bf117e353cc1ba8d1b6f795726f7ed10
Konstantyn <gagarin2789@gmail.com>
2024-05-17 00:11:36 +0200

    Implemented getUserListFromSearchedText function

--- app/src/main/java/com/clipie/main/data/repository/ChatRepositoryImpl.kt
+++ app/src/main/java/com/clipie/main/data/repository/ChatRepositoryImpl.kt
@@ -1,9 +1,11 @@
 package com.clipie.main.data.repository
 
+import com.clipie.authentication.domain.models.User
 import com.clipie.main.domain.model.Chat
 import com.clipie.util.FireStoreTable
 import com.clipie.main.domain.model.Message
 import com.clipie.main.domain.repository.ChatRepository
+import com.clipie.util.FireStoreField
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
@@ -18,7 +20,7 @@ class ChatRepositoryImpl @Inject constructor(
     override suspend fun getChatListFromCurrentUser(): List<Chat>? {
         return try {
             fireStore.collection(FireStoreTable.CHAT.tableName)
-                .whereArrayContains("usersId", firebaseAuth.currentUser?.uid.orEmpty())
+                .whereArrayContains(FireStoreField.USERSID.fieldName, firebaseAuth.currentUser?.uid.orEmpty())
                 .get()
                 .await()
                 .toObjects(Chat::class.java)
@@ -31,12 +33,25 @@ class ChatRepositoryImpl @Inject constructor(
     override suspend fun getMessagesFromChat(chat: Chat): List<Message>? {
         return try {
             fireStore.collection(FireStoreTable.MESSAGE.tableName)
-                .whereEqualTo("chatId", chat.id)
+                .whereEqualTo(FireStoreField.CHAT_ID.fieldName, chat.id)
                 .get()
                 .await()
                 .toObjects(Message::class.java)
         } catch (e: Exception) {
             if(e is CancellationException) throw e
+            null
+        }
+    }
+
+    override suspend fun getUserListFromSearchedText(searchText: String): List<User>? {
+        return try {
+            fireStore.collection(FireStoreTable.USER.tableName)
+                .whereIn(FireStoreField.USERNAME.fieldName, searchText.split(" "))
+                .get()
+                .await()
+                .toObjects(User::class.java)
+        } catch (e: Exception) {
+            if (e is CancellationException) throw e
             null
         }
     }
