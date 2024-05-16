cc57c636c257359364f67e4bf13d5be28e0dcff0
Konstantyn <gagarin2789@gmail.com>
2024-05-17 00:08:54 +0200

    Added new navigation constants

--- app/src/main/java/com/clipie/main/presentation/navigation/MainNavConstant.kt
+++ app/src/main/java/com/clipie/main/presentation/navigation/MainNavConstant.kt
@@ -2,7 +2,7 @@ package com.clipie.main.presentation.navigation
 
 enum class MainNavConstant(val route: String) {
     Home("home"),
-    HomeNavigation("home_navigation"),
+    ChatNavigation("chat_navigation"),
     Search("search"),
     Upload("upload"),
     Clips("clips"),
